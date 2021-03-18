98ba941e757c5e7e4dd6bd993815493caaf34273
saeed <fatemilg@yahoo.com>
2021-03-18 12:21:57 +0330

    991228-2

---
+++ src/app/vm-models/vm-action-detail.ts
@@ -0,0 +1,13 @@
+export class VM_Action_Detail {
+
+    //recieved
+    ContarctNumber :number;
+    OrderNumber :number;
+    Count:number;
+    TaskTitle : string;
+    StartDateAction : string;
+    EndDateAction:string;
+    CalculateDoneWorkTime:number;
+    CalculateExceptionSystemTime:number;
+
+}
